98f5f3d27d5c5cca2f59e7bb72674fe4c6d4d6dd
leviminch04 <54951562+leviminch04@users.noreply.github.com>
2020-01-21 09:23:33 -0800

    qew8yewr;ohdaisld

--- src/Solo/keyDetecter.java
+++ src/Solo/keyDetecter.java
@@ -1,15 +1,21 @@
 package Solo;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
+import javafx.util.Duration;
+
+import java.sql.Time;
 
 public class keyDetecter extends Application
 {
@@ -20,7 +26,8 @@ public class keyDetecter extends Application
         final int[] move = {10};
         int width = 300;
         int height = 300;
-        int circleRadius = 10;
+        int circleRadius = 20;
+        final int ENEMYMOVE = 1;
         Button button = new Button("");
         //button.setVisible(false);
         button.setShape(new Circle(0));
@@ -29,20 +36,17 @@ public class keyDetecter extends Application
         circle.setCenterX(circleX[0]);
         circle.setCenterY(circleY[0]);
         circle.setRadius(circleRadius);
+        circle.setStroke(Color.BLUE);
+        Circle enemy = new Circle();
+        enemy.setCenterX((double) (Math.random() * width));
+        enemy.setCenterY((double) (Math.random() * height));
+        enemy.setRadius(circleRadius);
+        enemy.setFill(Color.BLACK);
+        enemy.setStroke(Color.RED);
         button.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
             @Override
             public void handle(KeyEvent event) {
-                if(event.getCode() == KeyCode.SHIFT)
-                {
-                    move[0] = 100;
-                }
-
-                if(event.getCode() != KeyCode.SHIFT)
-                {
-                    move[0] = 10;
-                }
-
                 if(event.getCode() == KeyCode.A)
                 {
                     circleX[0] -= move[0];
@@ -66,24 +70,43 @@ public class keyDetecter extends Application
                     circleY[0] -= move[0];
                     circle.setCenterY(circleY[0]);
                 }
-
-                if(circle.getCenterX() >= width)
-                {
-                    circle.setCenterX(0);
-                    System.out.println(circle.getCenterX());
-                }
-
-//                if(circle.getCenterX() <= 0)
-//                {
-//                    circle.setCenterX(width);
-//                }
-
-
             }
         });
 
+        Timeline enemyMovement = new Timeline();
+        enemyMovement.setCycleCount(Timeline.INDEFINITE);
+        enemyMovement.getKeyFrames().add(
+                new KeyFrame(Duration.seconds((double) 1 / 60), event -> {
+                    if(circle.getCenterX() > enemy.getCenterX()){
+                        enemy.setCenterX((int) (enemy.getCenterX()) + ENEMYMOVE);
+                        System.out.println("enemy + x");
+                    }
 
-        Group root = new Group(button, circle);
+                    if(circle.getCenterX() < enemy.getCenterX()){
+                        enemy.setCenterX( (int) (enemy.getCenterX()) - ENEMYMOVE);
+                        System.out.println("enemy - x");
+                    }
+
+                    if(circle.getCenterY() > enemy.getCenterY()){
+                        enemy.setCenterY((int) (enemy.getCenterY()) + ENEMYMOVE);
+                        System.out.println("enemy + y");
+                    }
+
+                    if(circle.getCenterY() < enemy.getCenterY()){
+                        enemy.setCenterY( (int) (enemy.getCenterY()) - ENEMYMOVE);
+                        System.out.println("enemy - Y");
+                    }
+                    //System.exit(-1);
+
+
+                })
+
+        );
+
+        enemyMovement.playFromStart();
+
+
+        Group root = new Group(button, circle, enemy);
         Scene scene = new Scene(root, width, height);
 
         stage.setTitle("Key Detecter");
